9b548419d1d2e17a6513490f95b89c82bf044b68
Ives Bao <ives.bao@softtek.com>
2019-11-19 11:23:39 +0800

    add code

---
+++ ConsoleApp1/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class T
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+
+    }
+
+    public class T2
+    {
+        public string a { get; set; }
+        [Description("b")]
+        public int bb { get; set; }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapUtility;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new T { a = 1, b = 2 };
+            var t2 = t.Map<T2>();
+            Console.WriteLine(JsonConvert.SerializeObject(t2));
+
+            var list1 = new List<T> { new T{a=1,b=2},new T { a = 3, b = 4 } };
+            IList<T2> list2 = list1.MapList<T, T2>();
+            Console.WriteLine(JsonConvert.SerializeObject(list2));
+        }
+    }
+}
