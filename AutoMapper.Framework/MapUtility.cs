e6314decf2554b13e211bf77a85708ea8c883408
ives <renhanlinbsl@163.com>
2020-03-06 14:22:36 +0800

    add mapper for .net framework

---
+++ AutoMapper.Framework/MapUtility.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapper.Framework
+{
+    public static class MapUtility
+    {
+        public static T Map<T>(this object obj) where T : new()
+        {
+            var result = new T();
+            var properties = obj.GetType().GetProperties();
+
+            var resultProperties = result.GetType().GetProperties();
+            foreach (var i in properties)
+            {
+                foreach (var j in resultProperties)
+                {
+                    try
+                    {
+                        var atts = j.GetCustomAttributes(false);
+                        foreach (var item in atts)
+                        {
+                            if (item.GetType() == typeof(DescriptionAttribute))
+                            {
+                                var desName = ((DescriptionAttribute)item).Description;
+                                if (i.Name == desName)
+                                {
+                                    var value = Convert.ChangeType(i.GetValue(obj), j.PropertyType);
+                                    j.SetValue(result, value);
+                                    break;
+                                }
+                            }
+                        }
+                        if (i.Name == j.Name)
+                        {
+                            var value = Convert.ChangeType(i.GetValue(obj), j.PropertyType);
+                            j.SetValue(result, value);
+                            break;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        j.SetValue(result, Activator.CreateInstance(j.PropertyType));
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static IList<TOut> MapList<TIn, TOut>(this List<TIn> list) where TOut : new()
+        {
+            var result = new List<TOut>();
+            foreach (var item in list)
+            {
+                try
+                {
+                    result.Add(item.Map<TOut>());
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+            }
+            return result;
+        }
+    }
+}
